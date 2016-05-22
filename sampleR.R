22c91fffe124b169888d2394cd33ff3814b0669b
abharath9 <abharath9@gmail.com>
2016-05-22 15:55:01 -0400

    Data mining projects and concepts

---
+++ sampleR.R
@@ -0,0 +1,10 @@
+> library(twitteR)
+> library(wordcloud)
+> library(RColorBrewer)
+> library(plyr)
+> library(ggplot2)
+> library(sentiment)
+
+# Find OAuth settings for twitter:
+> library(httr)
+> oauth_endpoints(“twitter”)
